a5b00f469bae112ab09d46128e29a6bb2810f3df
vaekek <vfrusconi@gmail.com>
2018-03-22 16:40:12 +0100

    #resolves #1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Popular from './Popular';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
+const ReactRouter = require('react-router-dom');
+const Switch = ReactRouter.Switch;
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
 
 class App extends React.Component{
   render(){
     return(
-    <div className= "container">
-     <Popular/> 
-    </div>
+    <Router>
+      <div className= "container">
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/battle' component={Battle} />
+          <Route path='/popular' component={Popular} />
+          <Route render={function(){
+            return <p>Not Found 404</p>
+            
+          }}/>
+        </Switch>
+      </div>
+    </Router>
+    
     );
   }
 }
